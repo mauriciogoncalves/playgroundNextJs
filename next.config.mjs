3f97bf5c5ec504a0be1b779a96d5cea3703c47fe
Mauricio <mauricio.goncalves@gmail.com>
2024-03-26 19:50:45 +0100

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+import withBundleAnalyzer from "@next/bundle-analyzer"
+import withPlugins from "next-compose-plugins"
+import { env } from "./env.mjs"
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+  // output: 'export',
+  reactStrictMode: true,
+  experimental: { instrumentationHook: true },
+  rewrites() {
+    return [
+      { source: "/healthz", destination: "/api/health" },
+      { source: "/api/healthz", destination: "/api/health" },
+      { source: "/health", destination: "/api/health" },
+      { source: "/ping", destination: "/api/health" },
+    ]
+  },
+  env: {
+    customKey: 'my-value',
+  },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
+})
+
+export default config
