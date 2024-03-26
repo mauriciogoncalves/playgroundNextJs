fe8a28453b3347dc7e8ec525c596e2978c594c0c
Mauricio <mauricio.goncalves@gmail.com>
2024-03-27 00:04:37 +0100

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Web() {
                      className="select-none pointer-events-none mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
                     <div className="select-none pointer-events-none  mx-auto place-self-center">
                         <h1 className="select-none pointer-events-none mb-4 max-w-2xl text-4xl font-extrabold text-gray-500 leading-none tracking-tight dark:text-gray- md:text-5xl xl:text-6xl">
-                            Next.js Enterprise Playground
+                            Playground<br />Next.js Enterprise
                         </h1>
                         <p className="select-none  pointer-events-none  mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus eget nunc scelerisque viverra. Ante metus dictum at tempor commodo ullamcorper a lacus vestibulum. Sed turpis tincidunt id aliquet risus feugiat.
